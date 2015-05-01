d55b5b322364c54e1cb46ed46c2d93a40125a171
Bikrant Neupane <bneupane@Locals-MacBook-Pro.local>
2015-05-01 14:46:33 -0700

    Customize AWS OpsWorks recepies
    Testing AWS OpsWorks

---
+++ taw/metadata.rb
@@ -0,0 +1,8 @@
+name        "taw"
+description "TAW Generic configuration"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "apache2"
+depends "deploy"
---
+++ webapp/metadata.rb
@@ -0,0 +1,7 @@
+name        "webapp"
+description "Installs the environment for custom webapp"
+maintainer  "Bikrant Neupane"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "deploy"
